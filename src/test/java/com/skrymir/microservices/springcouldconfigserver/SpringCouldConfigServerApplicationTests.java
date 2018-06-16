2c5c0e0feeb5736cc5021a7ebb3aacc027e719f2
Joshua Colbeck <skrymir@gmail.com>
2018-06-15 23:01:40 -0500

    Initial commit

---
+++ src/test/java/com/skrymir/microservices/springcouldconfigserver/SpringCouldConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skrymir.microservices.springcouldconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCouldConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
